1599af1d058b212dd5a693cb59ce013c26cb70a3
Patrick <prjr25@gmail.com>
2020-12-02 19:02:54 -0800

    gestures

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { postComment } from '../redux/ActionCreators';
-import { Text, View, ScrollView, FlatList } from 'react-native';
+import { Text, View, ScrollView, FlatList, Alert, PanResponder } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -25,9 +25,49 @@ function RenderCampsite(props) {
 
     const {campsite} = props; 
 
+    const view = React.createRef();
+
+    const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            view.current.rubberBand(1000)
+            .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
+        },
+        onPanResponderEnd: (e, gestureState) => {
+            console.log('pan responder end', gestureState);
+            if (recognizeDrag(gestureState)) {
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + campsite.name + ' to favorite?',
+                    [
+                        {
+                            text: 'Cancel',
+                            style: 'cancel',
+                            onPress: () => console.log('Cancel Pressed')
+                        },
+                        {
+                            text: 'OK',
+                            onPress: () => props.favorite ? 
+                                console.log('Already set as a favorite') : props.markFavorite()
+                        }
+                    ],
+                    { cancelable: false }
+                );
+            }
+            return true;
+        }
+    });
+
     if (campsite) {
         return (
-                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Animatable.View
+                    animation='fadeInDown' 
+                    duration={2000} 
+                    delay={1000}
+                    ref={view}
+                    {...panResponder.panHandlers}>
                     <Card
                         featuredTitle={campsite.name}
                         image={{uri: baseUrl + campsite.image}}
